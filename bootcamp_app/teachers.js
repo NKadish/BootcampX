6f4895edab822bfab704725cf3a1eda452e41b6a
NKadish <noahkadish@hotmail.com>
2020-10-14 22:39:25 +0000

    practice postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const args = process.argv.slice(2);
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${args[0] || 'JUL02'}'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+});
